2328c818f48f57e6083ffb856c82331451caff59
yuuki798 <chengaopeng@linctex.com>
2025-06-10 13:59:16 +0800

    chore: config示例

--- domain/redis_client.go
+++ domain/redis_client.go
@@ -10,17 +10,10 @@ import (
 
 // 初始化 Redis 并自动 ping 测试
 func InitRedisClient() *redis.Client {
-	if viper.IsSet("redis.addr") == false {
-		log.Println("❌ redis.addr未配置，请检查配置文件")
-		return nil
-	}
-	if viper.IsSet("redis.password") == false {
-		log.Println("❌ redis.password未配置，请检查配置文件")
-		return nil
-	}
-	if viper.IsSet("redis.db") == false {
-		log.Println("❌ redis.db未配置，请检查配置文件")
-		return nil
+	if !viper.IsSet("redis.addr") ||
+		!viper.IsSet("redis.password") ||
+		!viper.IsSet("redis.db") {
+		log.Fatalln("❌ Redis 配置未设置，请检查配置文件")
 	}
 	addr := viper.GetString("redis.addr")
 	password := viper.GetString("redis.password")
